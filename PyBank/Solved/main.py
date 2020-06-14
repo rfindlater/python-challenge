c3527562b1b74c1e05131342e24b1e8320ad7c71
rfindlater <61257819+rfindlater@users.noreply.github.com>
2020-06-14 16:40:12 -0700

    adding original docs

---
+++ PyBank/Solved/main.py
@@ -0,0 +1,41 @@
+import os
+import csv
+
+bd_csv = os.path.join("..", "Resources", "budget_data.csv")
+
+#holders for data
+months=[]
+profitsLoss=[]
+
+
+# Open and read csv
+with open(bd_csv) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    
+    # Read through each row of data after the header
+    for row in csvreader:
+        months.append(str(row[0]))
+        profitsLoss.append(str(row[1]))
+        #print(profitsLoss)
+
+#months in column
+totalMonths=len(months)
+print(f"Total Months:{int(totalMonths)}")
+##proflost=sum(profitsLoss)
+##print(f"money:{int(proflost)}")
+
+
+#net Profit/Losses in column
+#netProfLoss=0
+
+#for x in profitsLoss=netProfLoss + x
+
+#average Profit/Losses
+#averageChange=[]
+#previousMonth=0
+
+#for x in range(len(profitsLoss)):
+    #if x ==0:
+        #previousMonth=profitsLoss[x]
+    #else
+        
